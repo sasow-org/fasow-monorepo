03857452d9256c17bb7dd16f48603b5f6800abb0
D1eku <dieku25ikari@gmail.com>
2022-10-22 17:27:31 -0400

    delete: unused files

--- packages/fasow/src/scenarios/ScenarioStrategy.ts
+++
@@ -1,5 +0,0 @@
-import MetaExperimentConfig from "../experiment/MetaExperimentConfig";
-
-export default interface ScenarioStrategy {
-  doStrategy(config: MetaExperimentConfig): void;
-}
--- packages/fasow/src/scenarios/twitter/TwitterStrategy.ts
+++
@@ -1,6 +0,0 @@
-import MetaExperimentConfig from "../../experiment/MetaExperimentConfig";
-import ScenarioStrategy from "../ScenarioStrategy";
-
-export default class TwitterStrategy implements ScenarioStrategy {
-  doStrategy(config: MetaExperimentConfig): void {}
-}
