6f4c6ce2ae9d192fbd1388cf4e1a04e445649479
D1eku <dieku25ikari@gmail.com>
2022-11-15 22:38:52 -0300

    feat: comment about  the main

--- packages/fasow/src/main.ts
+++ packages/fasow/src/main.ts
@@ -5,12 +5,25 @@ import IDataHandler from "./fasow/datahandler/IDataHandler";
 import ITowerHandler from "./fasow/reflection/tower/ITowerHandler";
 import ITimeKeeper from "./fasow/timekepper/ITimeKeeper";
 
+// FASOW es el sistema que se debe exportar el cual tiene al backend
 const fasow: FASOW = new FASOW();
+/*
+    Los siguientes tres exports corresponden a
+    DataHandler : Permite generar el output y el estado del backend.
+    TowerHandler : La torre de reflexion que permite controlar la arquitectura
+    TimeKepper : Modulo que maneja el tiempo (ticks y repeticiones)
+
+    Estos 3 exports son utilizados por el backend para funcionar
+    y no debieran ser usados por el frontend.
+
+ */
 export const DataHandler: IDataHandler = fasow.getDataHandler();
 export const TowerHandler: ITowerHandler = fasow.getTowerHandler();
 export const TimeKeeper: ITimeKeeper = fasow.getTimeKeeper();
-// fasow.runExperiment(ExampleExperiment);
+
 fasow.registerNewExperiment(MessageRepetition);
 fasow.registerNewExperiment(ExperimentAgentCombination);
 fasow.runExperimentByName("ExperimentAgentCombination");
 // console.log(fasow.getState());
+
+export function loadJSON() {}
