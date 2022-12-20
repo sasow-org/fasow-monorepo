e25c27ec9a7ce3763947f335bca197697e664911
txvnt <tomas.velez@alumnos.ucn.cl>
2022-12-20 12:23:18 -0300

    fix: removed js rule from gitignore

---
+++ packages/client/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require("webpack");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      react: "React",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  entry: "./src/index.tsx",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+};
