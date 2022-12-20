31c297508291ea8742a1c5ac0625dcb99e979e4c
txvnt <tomas.velez@alumnos.ucn.cl>
2022-12-19 22:50:54 -0300

    fix: fixed deploy

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -5,6 +5,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+window.React = React;
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
